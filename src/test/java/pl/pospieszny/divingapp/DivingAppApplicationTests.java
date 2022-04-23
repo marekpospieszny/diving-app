9e096eb3d34f30432a05e44db45b9fd376f24afe
Marek <marek.pospieszny@gmail.com>
2022-04-23 13:04:34 +0200

    Initial commit

---
+++ src/test/java/pl/pospieszny/divingapp/DivingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pospieszny.divingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
